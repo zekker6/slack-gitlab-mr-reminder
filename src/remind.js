214c2fb72f229ff83ebdea224bc47e18b8042f3a
Zakhar Bessarab <zekker6@gmail.com>
2020-10-28 11:02:37 +0300

    add logging options before starting

--- src/remind.js
+++ src/remind.js
@@ -15,15 +15,19 @@ if (fs.existsSync(optionsFile)) {
     options = yaml.safeLoad(fs.readFileSync(optionsFile, 'utf-8'));
 }
 
-options.slack.webhook_url = options.slack.webhook_url || process.env.SLACK_WEBHOOK_URL;
-options.slack.channel = options.slack.channel || process.env.SLACK_CHANNEL;
+options.slack.webhook_url = options.slack.webhook_url || process.env['SLACK_WEBHOOK_URL'];
+options.slack.channel = options.slack.channel || process.env['SLACK_CHANNEL'];
 
-options.gitlab.access_token = options.gitlab.access_token || process.env.GITLAB_ACCESS_TOKEN;
-options.gitlab.group = options.gitlab.group || process.env.GITLAB_GROUP;
-options.gitlab.external_url = options.gitlab.external_url|| process.env.GITLAB_EXTERNAL_URL;
+options.gitlab.access_token = options.gitlab.access_token || process.env['GITLAB_ACCESS_TOKEN'];
+options.gitlab.group = options.gitlab.group || process.env['GITLAB_GROUP'];
+options.gitlab.external_url = options.gitlab.external_url|| process.env['GITLAB_EXTERNAL_URL'];
 
-options.mr.normal_mr_days_threshold = options.gitlab.normal_mr_days_threshold || process.env.GITLAB_NORMAL_MR_DAYS_THRESHOLD;
-options.mr.wip_mr_days_threshold = options.gitlab.wip_mr_days_threshold || process.env.GITLAB_WIP_MR_DAYS_THRESHOLD;
+options.mr.normal_mr_days_threshold = options.gitlab.normal_mr_days_threshold || process.env['GITLAB_NORMAL_MR_DAYS_THRESHOLD'];
+options.mr.wip_mr_days_threshold = options.gitlab.wip_mr_days_threshold || process.env['GITLAB_WIP_MR_DAYS_THRESHOLD'];
+
+console.log('Parsed options: ' + JSON.stringify(options));
+console.log('Env options: ' + JSON.stringify(process.env));
+
 const reminder = new SlackGitlabMRReminder(options);
 
 reminder.remind();
