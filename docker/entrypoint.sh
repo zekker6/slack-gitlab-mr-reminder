6931bd4d1e63e73ee727f20dde80f542c2ec808a
Zakhar Bessarab <zekker6@gmail.com>
2020-10-28 13:26:55 +0300

    fix docker packaging for env vars

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+set -ex
 # Save process env
-env > /tmp/env
+(env | grep -E 'GITLAB|SLACK' | grep -v REMINDER_CRON_SCHEDULE | tr '\r\n' ' ') > /tmp/env
 
-echo "$REMINDER_CRON_SCHEDULE root . /tmp/env; /usr/local/bin/node /opt/app/src/remind.js $REMINDER_CONFIG_PATH >> /var/log/cron.log 0>&1" >> /etc/cron.d/reminder
+echo "$REMINDER_CRON_SCHEDULE root (`env | grep -E 'GITLAB|SLACK' | grep -v REMINDER_CRON_SCHEDULE | tr '\r\n' ' '`  /usr/local/bin/node /opt/app/src/remind.js $REMINDER_CONFIG_PATH ) > /proc/1/fd/1 2>/proc/1/fd/2" >> /etc/cron.d/reminder
 
-cron && tail -f /var/log/cron.log
+chmod 0644 /etc/cron.d/reminder
+crontab /etc/cron.d/reminder
+
+cron -f 
