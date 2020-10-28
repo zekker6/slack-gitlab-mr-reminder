eb7829cc8922caac2f9df78d233b169005613e17
Zakhar Bessarab <zekker6@gmail.com>
2020-10-28 14:50:33 +0300

    add http env for cron process

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 set -ex
-# Save process env
-(env | grep -E 'GITLAB|SLACK' | grep -v REMINDER_CRON_SCHEDULE | tr '\r\n' ' ') > /tmp/env
 
-echo "$REMINDER_CRON_SCHEDULE root (`env | grep -E 'GITLAB|SLACK' | grep -v REMINDER_CRON_SCHEDULE | tr '\r\n' ' '`  /usr/local/bin/node /opt/app/src/remind.js $REMINDER_CONFIG_PATH ) > /proc/1/fd/1 2>/proc/1/fd/2" >> /etc/cron.d/reminder
+echo "$REMINDER_CRON_SCHEDULE root (`env | grep -E 'GITLAB|SLACK|PROXY' | grep -v REMINDER_CRON_SCHEDULE | tr '\r\n' ' '`  /usr/local/bin/node /opt/app/src/remind.js $REMINDER_CONFIG_PATH ) > /proc/1/fd/1 2>/proc/1/fd/2" >> /etc/cron.d/reminder
 
 chmod 0644 /etc/cron.d/reminder
 crontab /etc/cron.d/reminder
