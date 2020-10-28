62f195e0364c81211200d8dd76ddfca4592d0071
Zakhar Bessarab <zekker6@gmail.com>
2020-10-28 11:48:26 +0300

    fix passing env variables to node process from docker cron (#1)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,6 @@
-echo "$REMINDER_CRON_SCHEDULE root /usr/local/bin/node /opt/app/src/remind.js $REMINDER_CONFIG_PATH >> /var/log/cron.log 2>&1" >> /etc/cron.d/reminder
+# Save process env
+env > /tmp/env
+
+echo "$REMINDER_CRON_SCHEDULE root . /tmp/env; /usr/local/bin/node /opt/app/src/remind.js $REMINDER_CONFIG_PATH >> /var/log/cron.log 0>&1" >> /etc/cron.d/reminder
 
 cron && tail -f /var/log/cron.log
